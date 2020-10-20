982bbd2748a596bc3ae0b0bee954ccc280b310d7
tugaykansu <tugaykansu@github.com>
2020-10-21 01:10:35 +0300

    fall

---
+++ Assets/cameraBeh.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class cameraBeh : MonoBehaviour
+{
+    
+    public GameObject Sphere;
+
+    private Vector3 diff;
+
+    void Start(){
+    	diff = transform.position - Sphere.transform.position;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.position = Sphere.transform.position + diff;
+    }
+}
