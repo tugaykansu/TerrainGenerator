0dfd3ee40d59ee60770f37994feb850943fc0016
tugaykansu <tugaykansu@github.com>
2020-10-21 16:48:29 +0300

    demo done

---
+++ Assets/Scripts/EnableIR.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnableIR : MonoBehaviour
+{
+	private CameraEffect ce;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        ce = Camera.main.GetComponent<CameraEffect>();
+    }
+
+    // Update is called once per frame
+    /*void Update()
+    {
+        
+    }*/
+
+    public void ToggleIR(){
+    	ce.m_Enable = !ce.m_Enable;
+    }
+}
